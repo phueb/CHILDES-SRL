822fdad58470f72283dfc42a7862b20ab01806c0
phueb <info@philhuebner.com>
2019-12-04 21:47:43 -0600

    add draft script to make test sentences

---
+++ make_test_sentences.py
@@ -0,0 +1,29 @@
+"""
+Adapted from Gerard & Mandler's "Ontological Knowledge and Sentence Anomaly"
+
+each stimuli set consists of terms and predicates.
+some preidcate-term combinations are semantically plausible, others are not.
+"""
+
+stimuli1 = {
+	'terms': [
+		'man',
+		'pig',
+		'car',
+		'milk',
+		'fight',
+		'sunrise',
+		'story',
+		'fear',
+	],
+	'predicates': [
+		'honest',
+		'asleep',
+		'broken',
+		'leaking out of boxes',
+		'somebody\'s fault',
+		'an hour long',
+		'about something',
+		'interesting',
+	]
+}
