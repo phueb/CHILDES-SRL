small fix: masking

babybertsrl/converter.py
@@ -93,10 +93,8 @@ class ConverterMLM:
                                                                                 self.wordpiece_tokenizer,
                                                                                 lowercase_input=False)
             # collect each multiple times, each time with a different masked word
-            num_wps = len(mlm_in_wp)
-            num_masked = min(num_wps, self.params.num_masked)
-            choices = np.arange(1, num_wps - 1)  # prevents masking of [SEP] or [CLS]
-            for masked_id in np.random.choice(choices, num_masked, replace=False):
+            choices = np.arange(1, len(mlm_in_wp) - 1)  # prevents masking of [SEP] or [CLS]
+            for masked_id in np.random.choice(choices, size=min(len(choices), self.params.num_masked), replace=False):
                 # mask
                 mlm_tags = mlm_in.copy()
                 mlm_tags_wp = [configs.Training.ignored_index if n != masked_id else self.wordpiece_tokenizer.vocab[w]
@@ -113,7 +111,7 @@ class ConverterMLM:
                                                   mlm_mask_wp)
                 res.append(instance)
 
-        print(f'With num_masked={self.params.num_masked}, made {len(res):>9,} MLM instances')
+        print(f'With num_sampled={self.params.num_masked}, made {len(res):>9,} MLM instances')
 
         return res
 

babybertsrl/model_mt.py
@@ -185,15 +185,11 @@ class MTBert(torch.nn.Module):
             # get max likelihood tags
             length = sequence_lengths[seq_id]
             tag_wp_probabilities = class_probabilities[seq_id].detach().cpu()[:length]
-
-            # TODO debug
-            print(class_probabilities.shape)
-            print(tag_wp_probabilities.shape)
-
             ml_tag_wp_ids, _ = viterbi_decode(tag_wp_probabilities, transition_matrix)  # ml = max likelihood
             ml_tags_wp = [self.id2tag_wp_srl[tag_id] for tag_id in ml_tag_wp_ids]
+
             # convert back from wordpieces
-            ml_tags = [ml_tags_wp[i] for i in  output_dict['start_offsets'][seq_id]]  # specific to BIO SRL tags
+            ml_tags = [ml_tags_wp[i] for i in output_dict['start_offsets'][seq_id]]  # specific to BIO SRL tags
             res.append(ml_tags)
 
         return res  # list of max likelihood tags